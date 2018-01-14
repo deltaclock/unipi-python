b476e2b8708e3967201c11155b1dd5b58abf6e9d
deltaclock <nickmour5@gmail.com>
2018-01-14 23:40:27 +0200

    Number2Roman

---
+++ int2roman.py
@@ -0,0 +1,25 @@
+romanNums = ((u'\u2188 ', 100000), (u'\u2187 ', 50000),
+             (u'\u2181 ', 10000), (u'\u2180 ', 5000),
+             ('M', 1000), ('CM', 900), ('D', 500),
+             ('CD', 400), ('C', 100), ('XC', 90),
+             ('L', 50), ('XL', 40), ('X', 10),
+             ('IX', 9), ('V', 5), ('IV', 4), ('I', 1))
+
+
+def int2roman(num):
+    resStr = ''
+    for letter, numValue in romanNums:
+        while num >= numValue:
+            resStr += letter
+            num -= numValue
+    return resStr
+
+# confirm with http://www.tuomas.salste.net/doc/roman/numeri-romani.html
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            print int2roman(int(raw_input('Enter a number ')))
+        except ValueError as e:
+            print 'ONLY numbers...'
