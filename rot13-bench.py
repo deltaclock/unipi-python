d3c3e00127b29b6df3da98547e354f3e744ddfc9
deltaclock <nickmour5@gmail.com>
2018-01-18 15:50:58 +0200

    Added a beta graph func

--- rot13-bench.py
+++ rot13-bench.py
@@ -6,6 +6,8 @@ import string
 import timeit
 from time import time
 import rot13
+import matplotlib.pyplot as plt
+import numpy as np
 
 '''
 ++++++++
@@ -34,7 +36,8 @@ number_of_runs = 10000
 ts = time()
 random_strings = [''.join(choice(string.printable) for _ in range(1000))
                   for __ in range(number_of_runs)]
-print 'Time required to make a list of 10000 strings ', time() - ts
+print 'Time required to make\
+ a list of {:d} strings {:.10f}'.format(number_of_runs, time() - ts)
 
 
 def benchLazy(text):
@@ -53,49 +56,79 @@ def benchrotNoneliner(text):
     rot13.rotNoneliner(13, text)
 
 
+def graph(performanceList):
+    # Restore the rc params from Matplotlib’s internal defaults.
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    funcs = ('Lazy', 'LazyV2', 'Math', 'String Methods')
+    y_pos = np.arange(len(funcs))
+    performance = performanceList
+    error = np.random.rand(len(funcs))
+
+    ax.barh(y_pos, performance, xerr=error, align='center',
+            color='yellow', ecolor='black')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(funcs)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('seconds (less is better)')
+    ax.set_title('Time each function took to complete')
+
+    plt.show()
+
+
 def tests():
-    if raw_input('This script will benchmark all 4 of the ROT functions..\
-                 \nThis might take a while.. Continue? [y/n]').lower() == 'y':
 
-        print 'Note the test will run 3 times x %i times for each test.. \
-So the result is the best of 3!\n' % number_of_runs
+    print 'Note the test will run 3 times x {} times for each test.. \
+So the result is the best of 3!\n'.format(number_of_runs)
 
-        lazy = '{:.10f}'.format(min(timeit.repeat(
-            "benchLazy(random_strings[i]); i += 1",
-            "from __main__ import benchLazy, random_strings; i = 0",
-            number=number_of_runs)))
+    lazy = min(timeit.repeat(
+        "benchLazy(random_strings[i]); i += 1",
+        "from __main__ import benchLazy, random_strings; i = 0",
+        number=number_of_runs))
 
-        print 'Using the dictionary we got ' + lazy
-        + ' sec for %i loops\n' % number_of_runs
+    print 'Using the dictionary we got {:d} sec\
+ for {:.10f} loops\n'.format(number_of_runs, lazy)
 
-        lazyV2 = '{:.10f}'.format(min(timeit.repeat(
-            "benchLazyV2(random_strings[i]); i += 1",
-            "from __main__ import benchLazyV2, random_strings; i = 0",
-            number=number_of_runs)))
+    lazyV2 = min(timeit.repeat(
+        "benchLazyV2(random_strings[i]); i += 1",
+        "from __main__ import benchLazyV2, random_strings; i = 0",
+        number=number_of_runs))
 
-        print 'Using the dict \w all the printable chars\
- we got ' + lazyV2 + ' sec for %i loops\n' % number_of_runs
+    print 'Using the dict \w all the printable chars\
+ we got {:d} sec for {:.10f} loops\n'.format(number_of_runs, lazyV2)
 
-        math = '{:.10f}'.format(min(timeit.repeat(
-            "benchMath(random_strings[i]); i += 1",
-            "from __main__ import benchMath, random_strings; i = 0",
-            number=number_of_runs)))
+    math = min(timeit.repeat(
+        "benchMath(random_strings[i]); i += 1",
+        "from __main__ import benchMath, random_strings; i = 0",
+        number=number_of_runs))
 
-        print 'Using the modulo we got ' + math
-        + ' sec for %i loops\n' % number_of_runs
+    print 'Using the modulo we got {:d} sec\
+ for {:.10f} loops\n'.format(number_of_runs, math)
 
-        strPacket = '{:.10f}'.format(min(timeit.repeat(
-            "benchrotNoneliner(random_strings[i]); i += 1",
-            "from __main__ import benchrotNoneliner, random_strings; i = 0",
-            number=number_of_runs)))
+    strPacket = min(timeit.repeat(
+        "benchrotNoneliner(random_strings[i]); i += 1",
+        "from __main__ import benchrotNoneliner, random_strings; i = 0",
+        number=number_of_runs))
 
-        print 'Using the string.translate module we got ' + strPacket +\
-            ' sec for %i loops\n' % number_of_runs
+    print 'Using the string.translate module we got {:d} sec\
+ for {:.10f} loops\n'.format(number_of_runs, strPacket)
+
+    return [lazy, lazyV2, math, strPacket]
 
 
 if __name__ == '__main__':
     try:
-        tests()
+        if raw_input('This script will benchmark all 4 of the ROT functions..\
+                     \nThis might take a while..\
+ Continue? [y/n] ').lower() == 'y':
+
+            res = tests()
+
+            if raw_input('Do you also want a\
+ diagram of the above numbers? [y/n] ').lower() == 'y':
+
+                graph(res)
     except (KeyboardInterrupt, EOFError):
         print '\nQuitting..'
         quit(0)
